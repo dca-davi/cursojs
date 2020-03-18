81d1e5193c48f6643576d4f3852dde10d1b6c9dd
dca-davi <dca_davi@hotmail.com>
2020-03-18 17:39:31 -0400

    Aula 13 curso js

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 10
+/*while (c < 6) {
+    console.log('Tudo bem?')
+    c++
+}*/
+
+/*do {
+    console.log(`Passo ${c}`)
+    c++
+
+}while (c <= 6)*/
+
+do {
+    console.log(`Contagem regressiva - ${c}`)
+    c--
+} while (c >= 0);
